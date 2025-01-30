3e5c96faa41f8c69c0a9e1c598f7d1f53902e263
Shobun Kollied Anwar <shobunkollieda@gmail.com>
2025-01-30 10:57:02 +0700

    first push

---
+++ app/c4ny0ug3tr1ch/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const AdminPage = () => {
+    return (
+        <main className="p-6">
+            <p className="text-gray-700">Welcome to the Admin Panel !</p>
+        </main>
+    );
+};
+
+export default AdminPage;
